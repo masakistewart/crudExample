29d198ba91f5ffa7d1b8f4a84390308a919544cf
Cairo Stewart <stewartcairo@gmail.com>
2015-12-19 10:53:41 -0800

    example complete... no ux

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ var bodyParser = require('body-parser');
 var app = express();
 var userRoutes = require('./controllers/students');
 var morgan = require('morgan');
-
+var methodOverride = require('method-override')
 app.get('/',function(req,res) {
-  res.redirect('/users');
+  res.redirect('/students');
  });
 
 app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/users', userRoutes);
+app.use('/students', userRoutes);
 app.listen(3000, function() {
 	console.log('server on port 3000');
 });
--- controllers/students.js
+++ controllers/students.js
@@ -17,20 +17,46 @@ router.get('/new',function(req,res) {
   res.render('pages/new');
 });
 
+//for editing user
+router.get('/:id/edit',function(req,res) {
+	knex('students').where({id: req.params.id}).then(function(data) {
+		res.locals.things = data;
+		res.render('pages/edit');
+	})
+ });
+
+router.put('/:id',function(req,res) { 
+	var name = req.body.userName
+  knex('students').where('id', req.params.id).update({name: name}).then(function() {
+  	console.log('done!')
+  	res.redirect('/students');
+  })
+ });
+
 // For New User
 router.post('/',function(req,res) {
 	var name = req.body.userName;
 	console.log(name);
 	knex('students').insert({name: name}).then(function(data){
-		res.redirect('/users');
+		res.redirect('/students');
 	}).catch(function(err){
 		console.log(err);
 	});
 });
 
+// for delete
+router.get('/:id/delete',function(req,res) { 
+  knex('students').where({id: req.params.id}).then(function(data) {
+		res.locals.things = data;
+		res.render('pages/delete');
+	})
+});
+
 // for Delete
-router.get('/delete',function(req,res) { 
-  
+router.delete('/:id',function(req,res) { 
+  knex('students').where('id', req.params.id).del().then(function() {
+  	res.redirect('/students');
+  })
  });
 
 module.exports = router;
